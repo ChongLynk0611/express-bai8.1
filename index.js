1aed6059e3694fd57aa87edbf37c8e10abad0d18
ChongLynk <tronglinhmta0611@gmail.com>
2020-07-12 07:21:43 -0400

    install modules

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 
 var express = require("express");
 var app = express();
-// set up pug
+// khai bao pug
 app.set('view engine', 'pug');
 app.set('views', './views');
+// khai bao lowdb
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
 
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+// tao file db.json mac dinh khi chua co file 
+
+db.defaults({  books : [] })
+  .write()
 
 
 
